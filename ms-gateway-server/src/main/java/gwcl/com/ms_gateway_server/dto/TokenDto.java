2770c12293ee99c38643e57242ea232949ec1452
23Vvrangler <leninbk17@gmail.com>
2024-11-30 20:39:35 -0500

    ms

---
+++ ms-gateway-server/src/main/java/gwcl/com/ms_gateway_server/dto/TokenDto.java
@@ -0,0 +1,14 @@
+package gwcl.com.ms_gateway_server.dto;
+
+import lombok.AllArgsConstructor;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+
+@AllArgsConstructor
+@Data
+public class TokenDto {
+    private String token;
+}
